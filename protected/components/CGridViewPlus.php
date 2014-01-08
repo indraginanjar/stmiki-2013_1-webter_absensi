80e531b2714aa4111a249877012ba9e7ec2ef720
indraginanjar <indraginanjar@gmail.com>
2014-01-08 10:29:37 +0700

    komponen CGridViewPlus diubah menjadi mengubah renderTableHeader() tetapi hasilnya lebar header menjadi kekecilan dibandingkan ukuran row filter dan datanya

--- protected/components/CGridViewPlus.php
+++ protected/components/CGridViewPlus.php
@@ -5,12 +5,70 @@ Yii::import('zii.widgets.grid.CGridView');
 class CGridViewPlus extends CGridView {
  
     public $addingHeaders = array();
- 
+	public $columnPerRow;
+	public $headerRow;
+
+	private function filterColumnByDataHeaderRow() {
+		
+	}
+
     public function renderTableHeader() {
-        if (!empty($this->addingHeaders))
-            $this->multiRowHeader();
+        if (!empty($this->addingHeaders)) {
+        	//$this->multiRowHeader();
+		//return;
+	}
  
-        parent::renderTableHeader();
+        //parent::renderTableHeader();
+	$colPos = 0;
+		if(!$this->hideHeader)
+		{
+			echo "<thead>\n";
+
+			if($this->filterPosition===self::FILTER_POS_HEADER)
+				$this->renderFilter();
+
+			$rows = array(array());
+			foreach($this->columns as $column) {
+				$rowNumber = 0;
+				if(isset($column->headerHtmlOptions['data-header_row'])){
+					$rowNumber = $column->headerHtmlOptions['data-header_row'];
+				}
+				$rows[$rowNumber][] = $column;
+			}
+			foreach($rows as $row) {
+				echo "<tr>\n";
+				foreach($row as $cell) {
+					$cell->renderHeaderCell();
+				}
+				echo "</tr>\n";
+			}
+			/*
+			foreach($this->columns as $column) {
+				$column->renderHeaderCell();
+				if(isset($column->headerHtmlOptions['colspan'])) {
+					$colPos += $column->headerHtmlOptions['colspan'];
+				}
+				else {
+					$colPos++;
+				}
+				if($colPos >= $this->columnPerRow) {
+					echo "</tr>\n<tr>";
+					$colPos = 0;
+				}
+			}
+			echo "</tr>\n";
+			*/
+			if($this->filterPosition===self::FILTER_POS_BODY)
+				$this->renderFilter();
+
+			echo "</thead>\n";
+		}
+		elseif($this->filter!==null && ($this->filterPosition===self::FILTER_POS_HEADER || $this->filterPosition===self::FILTER_POS_BODY))
+		{
+			echo "<thead>\n";
+			$this->renderFilter();
+			echo "</thead>\n";
+		}
     }
  
     protected function multiRowHeader() {
@@ -18,6 +76,20 @@ class CGridViewPlus extends CGridView {
         foreach ($this->addingHeaders as $row) {
             $this->addHeaderRow($row);
         }
+		if(!$this->hideHeader)
+		{
+
+			if($this->filterPosition===self::FILTER_POS_HEADER)
+				$this->renderFilter();
+
+			if($this->filterPosition===self::FILTER_POS_BODY)
+				$this->renderFilter();
+
+		}
+		elseif($this->filter!==null && ($this->filterPosition===self::FILTER_POS_HEADER || $this->filterPosition===self::FILTER_POS_BODY))
+		{
+			$this->renderFilter();
+		}
         echo CHtml::closeTag('thead') . "\n";
     }
  
@@ -28,11 +100,18 @@ class CGridViewPlus extends CGridView {
         // inherits header options from first column
         $options = $this->columns[0]->headerHtmlOptions;
         foreach ($row as $header) {
-            $options['colspan'] = $header['colspan'];
-            $cellOptions=($header['options'] + $options);
-            echo CHtml::openTag('th', $cellOptions);
-            echo $header['text'];
-            echo CHtml::closeTag('th');
+		if(isset($header['colspan'])) {
+			$options['colspan'] = $header['colspan'];
+		}
+		if(isset($header['options'])){
+			$cellOptions=($header['options'] + $options);
+		}
+		else {
+			$cellOptions=($options);
+		}
+		echo CHtml::openTag('th', $cellOptions);
+		echo $header['text'];
+		echo CHtml::closeTag('th');
         }
         echo CHtml::closeTag('tr') . "\n";
     }
