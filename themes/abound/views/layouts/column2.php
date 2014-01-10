3570a3eb11389722c27180769b6879db0e7070f3
indraginanjar <indraginanjar@gmail.com>
2014-01-10 17:33:34 +0700

    penghapusan menu contoh abound di menu samping (column2)

--- themes/abound/views/layouts/column2.php
+++ themes/abound/views/layouts/column2.php
@@ -9,15 +9,18 @@
 			/*'type'=>'list',*/
 			'encodeLabel'=>false,
 			'items'=>array(
-				array('label'=>'<i class="icon icon-home"></i>  Dashboard <span class="label label-info pull-right">BETA</span>', 'url'=>array('/site/index'),'itemOptions'=>array('class'=>'')),
+				array('label'=>'<i class="icon icon-home"></i>  Dashboard ', 'url'=>array('/site/index'),'itemOptions'=>array('class'=>'')),
+/*
 				array('label'=>'<i class="icon icon-search"></i> About this theme <span class="label label-important pull-right">HOT</span>', 'url'=>'http://www.webapplicationthemes.com/abound-yii-framework-theme/'),
 				array('label'=>'<i class="icon icon-envelope"></i> Messages <span class="badge badge-success pull-right">12</span>', 'url'=>'#'),
+*/
 				// Include the operations menu
 				array('label'=>'OPERATIONS','items'=>$this->menu),
 			),
 			));?>
 		</div>
         <br>
+<!--
         <table class="table table-striped table-bordered">
           <tbody>
             <tr>
@@ -68,7 +71,7 @@
               
             </dl>
       </div>
-		
+	-->	
     </div><!--/span-->
     <div class="span9">
     
